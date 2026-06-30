bd923e1516256f94675d43ae3cf15f8364c04bef
caosgegeh <calistenia.brasil.experto@gmail.com>
2026-06-29 21:39:28 -0300

    Update and rename B1.c to Tabuada.c

--- Tabuada.c
+++ Tabuada.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-void Tabuada() {
+void main() {
     for (int i = 1; i < 11; i++) {
         printf("\n Tabuada do %d\n", i);
         for (int j = 1; j < 11; j++) {
             printf("%d * %d = %d\n", i,j,i*j);
         }
         printf("\n");
-
     }
-}
-
-int main() {
-    bool teste = false;
-    printf("%b\n", teste);
-    return 0;
 }
